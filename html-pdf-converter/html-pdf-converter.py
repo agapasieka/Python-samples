5c5c04823f9ccd62d8b6d0848528bd37684cdfde
Agnieszka Pasieka <61354571+agapasieka@users.noreply.github.com>
2024-08-31 14:52:27 +0100

    Update html-pdf-converter.py

--- html-pdf-converter/html-pdf-converter.py
+++ html-pdf-converter/html-pdf-converter.py
@@ -1,3 +1,3 @@
 from pyhtml2pdf import converter
 
-converter.convert('https://agapasieka.github.io/resume/', 'agnieszka-pasieka-cv.pdf')
+converter.convert('https://agapasieka.github.io/resume/', 'https://drive.google.com/drive/folders/1yNtTa0NAPB7ZpbjwfrKcNEh3qGVA34q-/agnieszka-pasieka-cv.pdf')
