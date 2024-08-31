72871a43284e40679754abac6fcbda9108b5393a
Agnieszka Pasieka <61354571+agapasieka@users.noreply.github.com>
2024-08-31 15:14:43 +0100

    Update html-pdf-converter-email.py

--- html-pdf-converter/html-pdf-converter-email.py
+++ html-pdf-converter/html-pdf-converter-email.py
@@ -1,1 +1,20 @@
+import yagmail
+from pyhtml2pdf import converter
 
+# Convert HTML to PDF
+converter.convert('https://agapasieka.github.io/resume/', 'agnieszka-pasieka-cv.pdf')
+
+# Email settings
+sender_email = '${{ secrets.EMAIL_ADDRESS }}'
+receiver_email = 'agipasieka79"gmail.com' 
+subject = 'Your updated CV in PDF'
+body = 'Please find attached your CV in PDF format.'
+
+# Send the email with the attachment
+yag = yagmail.SMTP(sender_email, '${{ secrets.EMAIL_PASSWORD }}')
+yag.send(
+    to=receiver_email,
+    subject=subject,
+    contents=body,
+    attachments='agnieszka-pasieka-cv.pdf'
+)
