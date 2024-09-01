4fa563ef9a590e9df8ca8d7b0ae56fa885a55708
Agnieszka Pasieka <61354571+agapasieka@users.noreply.github.com>
2024-09-01 13:05:19 +0100

    Update html-pdf-converter-email.py

--- html-pdf-converter/html-pdf-converter-email.py
+++ html-pdf-converter/html-pdf-converter-email.py
@@ -4,7 +4,7 @@ import yagmail
 from pyhtml2pdf import converter
 
 # Convert HTML to PDF
-converter.convert('https://github.io/resume/', 'cv.pdf')     # replace the web page link and pdf file name  
+converter.convert('https://github.io/resume/', 'cv.pdf')     # Replace the web page link and pdf file name  
 
 # Email settings
 sender_email = os.getenv('EMAIL_ADDRESS')
