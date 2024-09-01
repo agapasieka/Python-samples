bfac0546ebf0b38a275a36a3d2a31d587fd1d4a0
Agnieszka Pasieka <61354571+agapasieka@users.noreply.github.com>
2024-09-01 09:25:43 +0100

    Update html-pdf-converter-email.py

--- html-pdf-converter/html-pdf-converter-email.py
+++ html-pdf-converter/html-pdf-converter-email.py
@@ -1,3 +1,4 @@
+import os
 import yagmail
 from pyhtml2pdf import converter
 
@@ -5,13 +6,13 @@ from pyhtml2pdf import converter
 converter.convert('https://agapasieka.github.io/resume/', 'agnieszka-pasieka-cv.pdf')
 
 # Email settings
-sender_email = '${{ secrets.EMAIL_ADDRESS }}'
+sender_email = os.getenv('EMAIL_ADDRESS')
 receiver_email = 'agipasieka79@gmail.com' 
 subject = 'Your updated CV in PDF'
 body = 'Please find attached your CV in PDF format.'
 
 # Send the email with the attachment
-yag = yagmail.SMTP(sender_email, '${{ secrets.EMAIL_PASSWORD }}')
+yag = yagmail.SMTP(sender_email, os.getenv('EMAIL_PASSWORD'))
 yag.send(
     to=receiver_email,
     subject=subject,
